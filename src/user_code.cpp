7ff96aa53a55e862498ec3a155b87ef9276cb8b3
tuket <tuketet@gmail.com>
2020-06-10 19:25:34 +0200

    icosphere!

--- src/user_code.cpp
+++ src/user_code.cpp
@@ -12,12 +12,14 @@ mat2 rotY(float a)
 	};
 }
 
-constexpr int maxSubDivs = 8;
-static int subDivs = 6;
+constexpr int maxSubDivs = 6;
+static int subDivs = 3;
 static vec3* verts[maxSubDivs+1] = {};
 static int numVerts[maxSubDivs+1] = {};
 static int* inds[maxSubDivs+1] = {};
 static int numInds[maxSubDivs+1] = {};
+static bool wireframe = true;
+static bool enableNormalize = false;
 
 void generateIcospheres(int subDivs)
 {
@@ -30,13 +32,18 @@ void generateIcospheres(int subDivs)
 		2 + // top and bot verts
 		2 * 5 * (fnl-1) * fnl / 2 + // top and bot caps
 		5 * (fnl+1) * fnl; // middle
-	const int ni = numInds[subDivs] = 4 * numInds[subDivs-1];
+
+	const int ni = numInds[subDivs] = 3 * (20 * (1 << (2 * subDivs)));
 	vec3* vv = verts[subDivs] = new vec3[nv];
 	int* ii = inds[subDivs] = new int[ni];
-	vv[0] = verts[0][0];
+
+	int vi = 0;
+	auto addVert = [&](vec3 p) {
+		vv[vi++] = /*glm::normalize*/(p);
+	};
+	addVert(verts[0][0]);
 
 	using glm::mix;
-	int vvi = 1;
 	for(int l = 1; l < fnl; l++) {
 		const float vertPercent = float(l) / fnl;
 		for(int f = 0; f < 5; f++) {
@@ -44,8 +51,7 @@ void generateIcospheres(int subDivs)
 			const vec3 pRight = mix(verts[0][0], verts[0][1+(f+1)%5], vertPercent);
 			for(int x = 0; x < l; x++) {
 				const vec3 p = mix(pLeft, pRight, float(x) / l);
-				vv[vvi] = p;
-				vvi++;
+				addVert(p);
 			}
 		}
 	}
@@ -62,17 +68,11 @@ void generateIcospheres(int subDivs)
 			const vec3 right = mix(topRight, botRight, vertPercent);
 			for(int x = 0; x < fnl-l; x++) {
 				const vec3 p = mix(left, mid, float(x) / (fnl-l));
-				vv[vvi] = p;
-				if(f != 0)
-					vv[vvi] = {0,0,0};
-				vvi++;
+				addVert(p);
 			}
 			for(int x = 0; x < l; x++) {
 				const vec3 p = mix(mid, right, float(x) / l);
-				vv[vvi] = p;
-				if(f != 0)
-					vv[vvi] = {0,0,0};
-				vvi++;
+				addVert(p);
 			}
 		}
 	}
@@ -84,17 +84,96 @@ void generateIcospheres(int subDivs)
 			const vec3 pRight = mix(verts[0][6+(f+1)%5], verts[0][11], vertPercent);
 			for(int x = 0; x < fnl-l; x++) {
 				const vec3 p = mix(pLeft, pRight, float(x) / (fnl-l));
-				vv[vvi] = p;
-				vvi++;
+				addVert(p);
 			}
 		}
 	}
 
-	vv[vvi] = verts[0][11];
-	vvi++;
+	addVert(verts[0][11]);
 
-	printf("%d - %d\n", vvi, nv);
-	assert(vvi == nv);
+	int i = 0;
+	auto addTri = [&](int i0, int i1, int i2) {
+		ii[i++] = i0;
+		ii[i++] = i1;
+		ii[i++] = i2;
+		assert(i <= ni);
+	};
+
+	// top
+	for(int f = 0; f < 5; f++)
+		addTri(0, 1+f, 1+(f+1)%5);
+	
+	int rowOffset = 1;
+	for(int l = 1; l < fnl; l++) {
+		const int rowLen = l*5;
+		const int nextRowLen = (l+1)*5;
+		for(int f = 0; f < 5; f++) {
+			for(int x = 0; x < l; x++) {
+				const int topLeft = rowOffset + l*f + x;
+				const int topRight = rowOffset + (l*f + x + 1) % rowLen;
+				const int botLeft = rowOffset + rowLen + (l+1) * f + x;
+				addTri(
+					topLeft,
+					botLeft,
+					botLeft + 1);
+				addTri(
+					topLeft,
+					botLeft + 1,
+					topRight);
+			}
+			addTri(
+				rowOffset + (l*(f+1)) % rowLen, // review!
+				rowOffset + rowLen + (l+1) * f + l,
+				rowOffset + rowLen + ((l+1) * f + l + 1) % nextRowLen);
+		}
+		rowOffset += rowLen;
+	}
+
+	{ // middle
+		const int rowLen = 5 * fnl;
+		for(int l = 0; l < fnl; l++) {
+			for(int x = 0; x < rowLen; x++) {
+				const int topLeft = rowOffset + x;
+				const int topRight = rowOffset + (x+1) % rowLen;
+				const int botLeft = rowOffset + rowLen + x;
+				const int botRight = rowOffset + rowLen + (x+1) % rowLen;
+				addTri(topLeft, botLeft, topRight);
+				addTri(topRight, botLeft, botRight);
+			}
+			rowOffset += rowLen;
+		}
+	}
+
+	// bottom
+	for(int l = 0; l < fnl-1; l++) {
+		const int faceLen = fnl - l;
+		const int rowLen = faceLen*5;
+		const int nextRowLen = (faceLen-1)*5;
+		for(int f = 0; f < 5; f++) {
+			for(int x = 0; x < fnl-l-1; x++) {
+				const int topLeft = rowOffset + faceLen*f + x;
+				const int topRight = rowOffset + faceLen*f + x + 1;
+				const int botLeft = rowOffset + rowLen + (faceLen-1) * f + x;
+				const int botRight = rowOffset + rowLen + ((faceLen-1) * f + x + 1) % nextRowLen;
+				addTri(topLeft, botLeft, topRight);
+				addTri(topRight, botLeft, botRight);
+				//addTri(0,0,0);
+				//addTri(0,0,0);
+			}
+			addTri(
+				rowOffset + faceLen*(f+1) - 1,
+				rowOffset + rowLen + ((faceLen-1) * (f+1)) % nextRowLen,
+				rowOffset + faceLen*(f+1) % rowLen);
+		}
+		rowOffset += rowLen;
+	}
+	for(int f = 0; f < 5; f++)
+		addTri(nv-6+f, nv-1, nv-6 + (f+1)%5);
+
+	numInds[subDivs] = i;
+
+	printf("%d - %d\n", vi, nv);
+	assert(vi == nv);
 }
 
 void userInit()
@@ -164,6 +243,8 @@ void drawGui()
 		subDivs = glm::max(0, subDivs);
 		generateIcospheres(subDivs);
 	}
+	ImGui::Checkbox("wireframe", &wireframe);
+	ImGui::Checkbox("normalize", &enableNormalize);
 	ImGui::End();
 }
 
@@ -177,40 +258,31 @@ void userDraws(float dt)
 
 	pushColor(RED);
 
-	/*for(int i = 0; i < 5; i++) {
-		const int i1 = (i+1)%5;
-		drawLine(verts[0], verts[1+i]);
-		drawLine(verts[1+i], verts[1+i1]);
-		drawLine(verts[6+i], verts[6+i1]);
-		drawLine(verts[1+i], verts[6+i]);
-		drawLine(verts[6+i], verts[1+i1]);
-		drawLine(verts[11], verts[6+i]);
-	}*/
-
-	/*
-	generateIcospheres(subDivs);
-	pushColor({0, 1, 0, 0.25f});
-	for(int i = 0; i < numInds[subDivs]; i+=3) {
-		drawTriangle(verts[subDivs][inds[subDivs][i+0]],
-					 verts[subDivs][inds[subDivs][i+1]],
-					 verts[subDivs][inds[subDivs][i+2]]);
-	}
-	popColor();
-	*/
-	for(int i = 0; i < numVerts[subDivs]; i++)
+	/*for(int i = 0; i < numVerts[subDivs]; i++)
 	{
 		drawPoint(verts[subDivs][i]);
+	}*/
+
+	for(int i = 0; i < numInds[subDivs]; i+=3)
+	{
+		vec3 p0 = verts[subDivs][inds[subDivs][i]];
+		vec3 p1 = verts[subDivs][inds[subDivs][i+1]];
+		vec3 p2 = verts[subDivs][inds[subDivs][i+2]];
+		if(enableNormalize) {
+			p0 = glm::normalize(p0);
+			p1 = glm::normalize(p1);
+			p2 = glm::normalize(p2);
+		}
+			
+		if(wireframe) {
+			drawLine(p0, p1);
+			drawLine(p1, p2);
+			drawLine(p2, p0);
+		}
+		else {
+			drawTriangle(p0, p1, p2);
+		}
 	}
 
-/*
-	for(int i = 0; i < 5; i++) {
-		const int i1 = (i+1)%5;
-		drawTriangle(pentagon1[i], pentagon1[i1], topVert);
-		drawTriangle(pentagon1[i], pentagon2[i], pentagon1[i1]);
-		drawTriangle(pentagon2[i], pentagon2[i1], pentagon1[i1]);
-		drawTriangle(pentagon2[i], -topVert, pentagon2[i1]);
-		//drawTriangle(topVert, pentagon1[i0], pentagon1[i1]);
-	}
-*/
 	popColor();
 }
